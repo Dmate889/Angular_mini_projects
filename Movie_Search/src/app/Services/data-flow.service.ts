75b093614ae719f7f77a70de7262e18244ee4715
FireWolfy89 <dmate899@gmail.com>
2025-01-27 22:27:35 +0100

    data-flow service added to manage rxjs data flows between components

---
+++ Movie_Search/src/app/Services/data-flow.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataFlowService {
+
+  private dataSubject = new BehaviorSubject<any>(null);
+  data$ = this.dataSubject.asObservable();
+
+  setData(data: any): void{
+    return this.dataSubject.next(data);
+  }
+
+
+  getData(): any{
+    return this.dataSubject.getValue();
+  }
+}
