520022a1f09cfc422daffebcc2d08c3b6f31b083
FireWolfy89 <dmate899@gmail.com>
2025-02-05 23:37:49 +0100

    Plot component for providing more detailed information from each movie

---
+++ Movie_Search/src/app/Components/plot/plot.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-plot',
+  standalone: false,
+  
+  templateUrl: './plot.component.html',
+  styleUrl: './plot.component.css'
+})
+export class PlotComponent {
+
+}
